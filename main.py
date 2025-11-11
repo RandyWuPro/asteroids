0cc46aeaf25236782fcb0a4055240c7810f242d4
RandyWuPro <randy.b.wu@gmail.com>
2025-11-10 22:00:03 -0800

    Added spaceship for the player.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -12,12 +13,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     dt = 0
     clock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()   
         dt = clock.tick(60) / 1000
         
